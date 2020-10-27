9a88a698e55a21a5da87a08083e72baddb56d698
prat2908 <221910401033@gitam.in>
2020-10-27 15:06:42 +0530

    done

--- session #4.py
+++ session #4.py
@@ -1,3 +1,4 @@
+#basic functions to take a string and take its individual values and perform various actions with it
 st=input("enter a string: ")
 a = list(st)
 a.reverse()
---
+++ session #5.py
@@ -0,0 +1,3 @@
+cars = input('enter some cars:  ')
+for car in cars :
+    print(car)
